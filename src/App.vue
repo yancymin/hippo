e66849b5be7aebbcf25414c9615b65ed719a3e08
yancymin <senseslofter@gmail.com>
2019-02-24 18:58:45 +0800

    layout

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,160 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  background-color: #1B1A19;
 }
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+
+/* http://meyerweb.com/eric/tools/css/reset/
+   v4.0 | 20180602
+   License: none (public domain)
+*/
+
+html,
+body,
+div,
+span,
+applet,
+object,
+iframe,
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p,
+blockquote,
+pre,
+a,
+abbr,
+acronym,
+address,
+big,
+cite,
+code,
+del,
+dfn,
+em,
+img,
+ins,
+kbd,
+q,
+s,
+samp,
+small,
+strike,
+strong,
+sub,
+sup,
+tt,
+var,
+b,
+u,
+i,
+center,
+dl,
+dt,
+dd,
+ol,
+ul,
+li,
+fieldset,
+form,
+label,
+legend,
+table,
+caption,
+tbody,
+tfoot,
+thead,
+tr,
+th,
+td,
+article,
+aside,
+canvas,
+details,
+embed,
+figure,
+figcaption,
+footer,
+header,
+hgroup,
+main,
+menu,
+nav,
+output,
+ruby,
+section,
+summary,
+time,
+mark,
+audio,
+video {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  font-size: 100%;
+  font: inherit;
+  vertical-align: baseline;
+}
+
+/* HTML5 display-role reset for older browsers */
+
+article,
+aside,
+details,
+figcaption,
+figure,
+footer,
+header,
+hgroup,
+main,
+menu,
+nav,
+section {
+  display: block;
+}
+
+/* HTML5 hidden-attribute fix for newer browsers */
+
+*[hidden] {
+  display: none;
+}
+
+body {
+  line-height: 1;
+}
+
+ol,
+ul {
+  list-style: none;
+}
+
+blockquote,
+q {
+  quotes: none;
+}
+
+blockquote:before,
+blockquote:after,
+q:before,
+q:after {
+  content: "";
+  content: none;
+}
+
+table {
+  border-collapse: collapse;
+  border-spacing: 0;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,242 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <ul class="icon-wrap">
+        <li class="icon">
+          <a href="https://google.com">
+            <div class="img-wrap">
+              <img src="../assets/design-work.svg" alt>
+            </div>Work
+          </a>
+        </li>
+        <li class="icon">
+          <a href="https://google.com">
+            <div class="img-wrap">
+              <img src="../assets/zcool.svg" alt>
+            </div>站酷
+          </a>
+        </li>
+        <li class="icon">
+          <a href>/</a>
+        </li>
+        <li class="icon">
+          <a href="https://google.com">
+            <div class="img-wrap">
+              <img src="../assets/weibo1.svg" alt>
+            </div>Weibo
+          </a>
+        </li>
+        <li class="icon">
+          <a href="https://google.com">
+            <div class="img-wrap">
+              <img src="../assets/dribbble.svg" alt>
+            </div>Dribbble
+          </a>
+        </li>
+        <li class="icon">
+          <a href="https://google.com">
+            <div class="img-wrap">
+              <img src="../assets/ig.svg" alt>
+            </div>Instagram
+          </a>
+        </li>
+        <li class="icon">
+          <a href="https://google.com">
+            <div class="img-wrap">
+              <img src="../assets/Email.svg" alt>
+            </div>Email me
+          </a>
+        </li>
+        <li class="icon">
+          <a href="https://google.com">
+            <div class="img-wrap">
+              <img src="../assets/wechat.svg" alt>
+            </div>WeChat
+          </a>
+        </li>
+        <li class="icon">
+          <a href></a>
+        </li>
+      </ul>
+      <div class="dialog">
+        <ul class="dialog-wrap">
+          <li>
+            <img src="../assets/me.png" alt>
+            <p>Hi，You found me！</p>
+          </li>
+          <li>
+            <img src="../assets/me.png" alt>
+            <p>
+              I’m
+              <strong>Hwoma～</strong> Nice to meet you！
+            </p>
+          </li>
+          <li>
+            <img src="../assets/me.png" alt>
+            <p>
+              I’m a
+              <strong>designer</strong>.
+              <br>Based in
+              <strong>Wuhan, China</strong>
+            </p>
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+<style lang="scss" scoped>
+.home {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
-export default {
-  name: 'home',
-  components: {
-    HelloWorld,
-  },
-};
-</script>
+  .container {
+    position: relative;
+    width: 700px;
+    height: 400px;
+    background: url("../assets/screen-bg.jpg");
+    background-size: cover;
+
+    .icon-wrap {
+      position: absolute;
+      left: 215px;
+      top: 30px;
+      width: 224px;
+      height: 210px;
+      display: flex;
+      justify-content: space-between;
+      align-content: space-between;
+      flex-wrap: wrap;
+      li {
+        position: relative;
+        width: 64px;
+        height: 64px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        &:nth-of-type(3) {
+          visibility: hidden;
+          opacity: 0;
+        }
+        &:nth-of-type(9) {
+          visibility: hidden;
+          opacity: 0;
+        }
+
+        a {
+          display: flex;
+          justify-content: center;
+          flex-direction: column;
+          width: 100%;
+          height: 100%;
+          font-size: 12px;
+          font-weight: 600;
+          color: #ffffff;
+          letter-spacing: 0;
+          text-shadow: 0 0 1px rgba(0, 0, 0, 0.5);
+          text-decoration-line: none;
+          white-space: nowrap;
+          margin-top: 16px;
+          transform: scale(0.9);
+          cursor: pointer;
+
+          &::before {
+            z-index: -1;
+            opacity: 0;
+            content: "";
+            position: absolute;
+            left: -8px;
+            display: block;
+            width: 100%;
+            height: 100%;
+            background-color: #e1b9a7;
+            padding: 8px;
+            border-radius: 6px;
+            transition: all 0.2s ease-out;
+          }
+
+          &:hover::before {
+            opacity: 1;
+          }
+
+          .img-wrap {
+            width: 100%;
+            height: 100%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+          }
+
+          img {
+            margin-bottom: 6px;
+          }
+        }
+      }
+    }
+
+    .dialog {
+      z-index: 999;
+      opacity: 1;
+      /* position: absolute; */
+
+      .dialog-wrap {
+        li {
+          display: flex;
+          justify-content: flex-start;
+          align-items: center;
+          position: absolute;
+          padding: 8px 14px;
+          background-color: #464646;
+          border-radius: 6px;
+          color: #fff;
+          font-size: 12px;
+          text-align: left;
+          right: 0;
+          margin-right: 20px;
+
+          p {
+            transform: scale(0.9);
+          }
+
+          img {
+            width: 32px;
+            height: 32px;
+          }
+
+          strong {
+            font-weight: 800;
+          }
+
+          &::before {
+            content: "";
+            display: block;
+            position: absolute;
+            right: -5px;
+            bottom: 10px;
+            width: 10px;
+            height: 10px;
+            border-radius: 1px;
+            background-color: #464646;
+            transform: rotate(45deg);
+          }
+
+          &:nth-of-type(1) {
+            top: 20px;
+          }
+          &:nth-of-type(2) {
+            top: 75px;
+          }
+          &:nth-of-type(3) {
+            top: 130px;
+          }
+        }
+      }
+    }
+  }
+}
+</style>
